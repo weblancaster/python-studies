a143492021492e2947ed0c0e71fd95c7ac7abce2
Michael Lancaster <michaell.llancaster@gmail.com>
2018-01-13 23:31:23 -0800

    Day 12
    Finished implementing some simple endpoints using GET/POST
    and so far really enjoying Python even though I haven't really built anything.

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
-from flask import Flask, jsonify, request
+"""
+Main flask app
+"""
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-stores = [
+STORES = [
     {
         "name": "Nike town San Francisco",
         "items": [
             {
                 "name": "Metcon 3",
-                "price": 160
+                "price": 130
             }
         ]
     }
 ]
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 @app.route("/store")
 def get_stores():
     return jsonify({
-        "stores": stores
+        "data": STORES
     })
 
 @app.route("/store", methods=["POST"])
@@ -27,22 +34,55 @@ def create_store():
         "name": body["name"],
         "items": []
     }
-    stores.append(new_store)
+    STORES.append(new_store)
 
     return jsonify({
-        "stores": stores
+        "data": STORES
     })
 
 @app.route("/store/<string:name>")
 def get_store_by_name(name):
-    pass
+    formatted_name = name.replace("-", " ")
+    store = [store for store in STORES if store["name"] == formatted_name]
+
+    if not store:
+        return jsonify({
+            "message": "Store {} not found".format(formatted_name)
+        })
+
+    return jsonify({
+        "data": store
+    })
 
 @app.route("/store/<string:name>/item")
-def get_store_item_by_name(name):
-    pass
+def get_store_items_by_name(name):
+    formatted_name = name.replace("-", " ")
+    items = [store["items"] for store in STORES if store["name"] == formatted_name]
+
+    if not items:
+        return jsonify({
+            "message": "Store {} not found".format(formatted_name)
+        })
+    
+    return jsonify({
+        "data": items[0]
+    })
 
 @app.route("/store/<string:name>/item", methods=["POST"])
 def create_store_item_by_name(name):
-    pass
+    new_item = request.get_json()
+    formatted_name = name.replace("-", " ")
+    store = [store for store in STORES if store["name"] == formatted_name]
+
+    if not store:
+        return jsonify({
+            "message": "Store {} not found".format(formatted_name)
+        })
+    
+    store[0]["items"].append(new_item)
+
+    return jsonify({
+        "data": store
+    })
 
 app.run(port=5000)
